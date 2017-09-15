5675eb230cfa046000cf723a271eb7aa53b2b5cd
zhiyeTan <594557148@qq.com>
2017-09-16 00:13:43 +0800

    v1.02

--- z/core/Log.php
+++ z/core/Log.php
@@ -30,49 +30,79 @@ class Log
 		return self::$logsPath;
 	}
 	/**
-	 * 记录日志
+	 * 保存一条日志
 	 * 
 	 * @access public
+	 * @param  string  $strFileName             日志文件名
+	 * @param  string  $magicVal                当使用第三个参数时，作为分析型日志的key，否则作为记录型日志的内容
+	 * @param  string  $strAnalysisTypeContent  分析型日志要保存的内容
+	 * @return boolean
+	 */
+	public static function save($strFileName, $magicVal, $strAnalysisTypeContent = ''){
+		if($strAnalysisTypeContent){
+			self::saveAnalysisTypeLog($strFileName, $magicVal, $strAnalysisTypeContent);
+		}
+		else{
+			self::saveRecordTypeLog($strFileName, $magicVal);
+		}
+	}
+	/**
+	 * 保存记录型的日志
+	 * 
+	 * @access private
 	 * @param  string  $strFileName  日志文件名
 	 * @param  string  $strContent   单条日志的内容
 	 * @return boolean
 	 */
-	public static function save($strFileName, $strContent){
+	private static function saveRecordTypeLog($strFileName, $strContent){
 		$withoutSuffixPath = self::getLogsPath() . $strFileName;
 		$logPath = $withoutSuffixPath . '.txt';
-		// 如果文件存在且超过大小上限，则以当前时间重命名该文件
+		//如果文件存在且超过大小上限，则以当前时间重命名该文件
 		if(is_file($logPath) && filesize($logPath) > LOG_MAX_SIZE){
 			$newPath = $withoutSuffixPath . time() . '.txt';
-			// 设置一个值，防止出现死循环(一次延迟100毫秒，30次相当于3s)
+			//设置一个值，防止出现死循环(一次延迟100毫秒，30次相当于3s)
 			$domax = 30;
-			// 循环，直到成功或者超时
+			//循环，直到成功或者超时
 			$i = 0;
 			do{
 				++$i;
 				if(!rename($logPath, $newPath)){
-					usleep(100); // 延迟100毫秒
+					usleep(100); //延迟100毫秒
 				}
 			}
 			while(!$status && $i < $domax);
 		}
-		// 保存日志
-		return Basic::write($logPath, $strContent . PHP_EOL, false, true, false);
+		Basic::write($logPath, $strContent . PHP_EOL, false, true, false);
 	}
 	/**
-	 * 分为记录型recordType和分析型analysisType
-	 * 记录分析型日志
-	 * 每行的格式为MD5($strKey):$strContent . PHP_EOL
-	 * 其中$strContent必须包含一个可变值，其格式为[数字]
-	 * 若存在MD5($strKey)时则更新“[数字]”中的数字，否则插入到新行中
+	 * 保存分析型的日志
+	 * 保存新日志时自动在该行后面加上[1]
+	 * 保存一条已存在的记录时，则自动对对应行后面的数字累加并保存
 	 * 
-	 * @access public
+	 * @access private
 	 * @param  string  $strFileName  日志文件名
-	 * @param  string  $strKey       作为识别键用的唯一字符串，在日志中的MD5加密串
-	 * @param  string  $strValue     变更值，允许带运算符号，如：+、-、*、/
+	 * @param  string  $strKey       作为识别键用的唯一字符串
 	 * @param  string  $strContent   内容
 	 */
-	private static function saveAnalysisTypeLog($strFileName, $strContent, $strKey, $strValue){
+	private static function saveAnalysisTypeLog($strFileName, $strKey, $strContent){
 		$logPath = self::getLogsPath() . $strFileName . '.txt';
+		$logContent = Basic::read($logPath, false);
+		$realKey = md5($strKey);
+		$needAdd = true;
+		if($logContent){
+			$num = 1;
+			preg_match('/'.$realKey.':.*?\[(\d+)\]'.PHP_EOL.'/', $logContent, $result);
+			if(isset($result[1])){
+				$needAdd = false;
+				$num += (int)$result[1];
+				$newlogContent = preg_replace('/('.$realKey.':.*?)(\[\d+\])('.PHP_EOL.')/', '\1['.$num.']\3', $logContent);
+				Basic::write($logPath, $newlogContent, false);
+			}
+		}
+		if($needAdd){
+			$realContent = $realKey . ':' . $strContent . '[1]';
+			Basic::write($logPath, $realContent . PHP_EOL, false, true, false);
+		}
 	}
 	/**
 	 * 列出所有日志
